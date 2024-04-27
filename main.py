67f99dbb73283a23464ed3bbcf4aa6b484864bd9
AminIdr <aminidrissimaroc@gmail.com>
2024-04-27 14:35:30 +0100

    Priority based Round-Robin

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ from algorithms.priority_rr import priority_round_robin
 from models.process import Process
 # Generate random input data (processes)
 # num_processes = 5
-# num_priorities = (1, 3)
+# num_priorities = (1, 6)
 # burst_time_range = (3, 10)
 # arrival_time_range = (0, 8)
-# processes = generate_processes(num_processes, burst_time_range, arrival_time_range)
+# processes = generate_processes(num_processes, burst_time_range, arrival_time_range, num_priorities)
 
 # for process in processes:
 #     print(process)
 
-# print("#####################")
+print("#####################")
 # # Apply Priority scheduling algorithm
 # scheduled_processes, curr_time = round_robin(processes, 1)
 
@@ -34,22 +34,33 @@ from models.process import Process
 # for process in scheduled_processes:
 #     print(process)
 
-p1 = Process(1, 7, 8, 1)
-p2 = Process(2, 0, 9, 2)
-p3 = Process(3, 2, 5, 2)
-p4 = Process(4, 8, 9, 1)
-p5 = Process(5, 7, 5, 4)
+p1 = Process(1, 0, 12, 3)
+p2 = Process(2, 5, 19, 3)
+p3 = Process(3, 8, 21, 5)
+p4 = Process(4, 11, 13, 2)
+p5 = Process(5, 15, 15, 3)
 
 processes = [p1, p2, p3, p4, p5]
 # Apply Priority scheduling algorithm
-# scheduled_processes, curr_time = round_robin(processes, 3)
+scheduled_processes, curr_time = priority_round_robin(processes, 4)
 
 # # Display scheduled processes
-# print("Scheduled Processes (Round Robin Scheduling):")
-# for i in range(len(scheduled_processes)):
-#     print(scheduled_processes[i], curr_time[i])
+print("Scheduled Processes (Round Robin Scheduling):")
+for i in range(len(scheduled_processes)):
+    print(scheduled_processes[i], curr_time[i])
 
-# Apply SJF scheduling algorithm
-scheduled_processes = priority_round_robin(processes, 2)
-for process in scheduled_processes:
-    print(process)
+
+
+# p1 = Process(1, 6, 10, 2)
+# p2 = Process(2, 4, 3, 4)
+# p3 = Process(3, 3, 3, 1)
+# p4 = Process(4, 2, 6, 2)
+# p5 = Process(5, 5, 4, 2)
+# processes = [p1, p2, p3, p4, p5]
+# scheduled_processes = priority_preemptive(processes)
+# for process in scheduled_processes:
+#     print(process)  
+# scheduled_processes, curr_time = priority_round_robin(processes, 2)
+# for i in range(len(scheduled_processes)):
+#     # print("het")
+#     print(scheduled_processes[i], curr_time[i])
