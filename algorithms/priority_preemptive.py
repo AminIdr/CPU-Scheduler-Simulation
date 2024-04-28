d95cfa045413261ad201882f2b27530846951a9f
imanefjer <imane.fjer@gmail.com>
2024-04-28 18:06:32 +0100

    priority_preemptive

--- algorithms/priority_preemptive.py
+++ algorithms/priority_preemptive.py
@@ -7,29 +7,29 @@ def priority_preemptive(processes):
     queue = []
     execution_order = []
     remaining_time = {p.pid: p.burst_time for p in processes}
-
+    scheduler = []
     while completed_processes < n:
         available_processes = [p for p in processes if p.arrival_time <= current_time and remaining_time[p.pid] > 0]
         if available_processes:
             highest_priority_process = min(available_processes, key=lambda x: x.priority)
             queue.append(highest_priority_process)
-
-            if len(queue) > 1 and highest_priority_process != queue[-2]:
-                total_waiting_time += current_time - queue[-2].arrival_time
-
             remaining_time[highest_priority_process.pid] -= 1
             execution_order.append(highest_priority_process)
-
+            current_time += 1
             if remaining_time[highest_priority_process.pid] == 0:
                 completed_processes += 1
-                total_turnaround_time += current_time - highest_priority_process.arrival_time + 1
-                highest_priority_process.completion_time = current_time + 1
+                turnaround_time = current_time - highest_priority_process.arrival_time 
+                total_turnaround_time += turnaround_time
+                waiting_time = turnaround_time - highest_priority_process.burst_time
+                total_waiting_time += waiting_time
+                highest_priority_process.completion_time = current_time 
+                scheduler.append([highest_priority_process, turnaround_time, waiting_time])
 
-            current_time += 1
         else:
             current_time += 1
-
+    print("Total Waiting Time: ", total_waiting_time)
     avg_waiting_time = total_waiting_time / n
+    
     avg_turnaround_time = total_turnaround_time / n
 
-    return  execution_order, avg_waiting_time, avg_turnaround_time
+    return scheduler, execution_order, avg_turnaround_time, avg_waiting_time
